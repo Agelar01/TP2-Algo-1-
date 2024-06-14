ee758cb0ecce0f539ecbfa043d7c881a0a8ee2d9
lucaskendikian <132597686+lucaskendikian@users.noreply.github.com>
2024-06-14 23:46:53 +0000

    Add files via upload

---
+++ Diccionario.java
@@ -0,0 +1,15 @@
+public interface Diccionario {
+    
+    public boolean está(T clave);
+
+    public void definir(T clave, T valor);
+
+    public void definirRapido(T clave, T valor);
+
+    public T obtener(T clave);
+
+    public void borrar(T clave);
+
+    public int tamaño();
+
+}
